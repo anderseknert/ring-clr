1d1031db412e43ecb87f2236c5875f4d9c7a1762
Anders Eknert <anders@eknert.com>
2023-01-19 09:51:11 +0100

    HEAD middleware
    Signed-off-by: Anders Eknert <anders@eknert.com>

---
+++ test/ring_clr/middleware/head_test.clj
@@ -0,0 +1,46 @@
+(ns ring-clr.middleware.head-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [ring-clr.middleware.head :refer [head-request head-response wrap-head]]))
+
+(defn- handler
+  ([req]
+   {:status 200
+    :headers {"X-method" (name (:request-method req))}
+    :body "Foobar"})
+  ([req cont raise]
+   (cont
+    {:status 200
+     :headers {"X-method" (name (:request-method req))}
+     :body "Foobar"})))
+
+(deftest test-wrap-head
+  (let [resp ((wrap-head handler) {:request-method :head})]
+    (is (nil? (:body resp)))
+    (is (= "get" (get-in resp [:headers "X-method"]))))
+  (let [resp ((wrap-head handler) {:request-method :post})]
+    (is (= (:body resp) "Foobar"))
+    (is (= "post" (get-in resp [:headers "X-method"])))))
+
+(deftest wrap-head-cps-test
+  (testing "HEAD request"
+    (let [response  (promise)
+          exception (promise)]
+      ((wrap-head handler) {:request-method :head} response exception)
+      (is (nil? (:body @response)))
+      (is (= "get" (get-in @response [:headers "X-method"])))
+      (is (not (realized? exception)))))
+
+  (testing "POST request"
+    (let [response  (promise)
+          exception (promise)]
+      ((wrap-head handler) {:request-method :post} response exception)
+      (is (= "Foobar" (:body @response)))
+      (is (= "post" (get-in @response [:headers "X-method"])))
+      (is (not (realized? exception))))))
+
+(deftest head-request-test
+  (is (fn? head-request)))
+
+(deftest head-response-test
+  (is (fn? head-response))
+  (is (nil? (head-response nil {:request-method :head}))))
--- test/ring_clr/test.clj
+++ test/ring_clr/test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [run-all-tests]]
             [ring-clr.core.protocols-test]
             [ring-clr.middleware.content-type-test]
+            [ring-clr.middleware.head-test]
             [ring-clr.util.mime-type-test]
             [ring-clr.util.request-test]
             [ring-clr.util.response-test]
