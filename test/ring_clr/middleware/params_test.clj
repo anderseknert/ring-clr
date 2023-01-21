f2238f96e581904a225b02512c6fb2b106b0d841
Anders Eknert <anders@eknert.com>
2023-01-22 00:18:00 +0100

    Params middleware
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- test/ring_clr/middleware/params_test.clj
+++ test/ring_clr/middleware/params_test.clj
@@ -38,8 +38,8 @@
     (is (= {} (:params resp)))))
 
 (deftest wrap-params-encoding
-  (let [req  {:headers {"content-type" "application/x-www-form-urlencoded;charset=UTF-16"}
-              :body (string-input-stream "hello=world" "UTF-16")}
+  (let [req  {:headers {"content-type" "application/x-www-form-urlencoded;charset=UTF-8"}
+              :body (string-input-stream "hello=world" "UTF-8")}
         resp (wrapped-echo req)]
     (is (= (:params resp) {"hello" "world"}))
     (is (= (:form-params resp) {"hello" "world"}))))
